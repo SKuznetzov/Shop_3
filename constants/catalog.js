c2ffa07741fdaf0a4b812d14305d7a14a662fcf0
SKuznetzov <s-kuznecov100@rambler.ru>
2021-09-24 13:42:56 +0300

    add catalog

---
+++ constants/catalog.js
@@ -0,0 +1,20 @@
+const CATALOG = [
+    {
+        id: '1',
+        name: 'black&decker',
+        img: 'https://images.unsplash.com/photo-1574269909862-7e1d70bb8078?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=876&q=80',
+        price: 0
+    },
+    {
+        id: '2',
+        name: 'sharp',
+        img: 'https://images.unsplash.com/photo-1585659722983-3a675dabf23d?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=870&q=80',
+        price: 0
+    },
+    {
+        id: '3',
+        name: 'sharp',
+        img: 'https://images.unsplash.com/photo-1585659722983-3a675dabf23d?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=870&q=80',
+        price: 0
+    },
+]
