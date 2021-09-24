2c47cfa72c9eff2cb5740ffc2f4b804dbc084d82
SKuznetzov <s-kuznecov100@rambler.ru>
2021-09-24 20:45:28 +0300

    writing styles

--- components/Products/Products.js
+++ components/Products/Products.js
@@ -4,15 +4,15 @@ class Products {
 
         CATALOG.forEach(({id,price,img,name})=>{
             htmlCatalog +=`
-            <li>
-                <span>${name}</span>
-                <img src="${img}" />
-                <span>${price}</span>
-                <button>В корзину</button>
+            <li class="products-element">
+                <span class="products-element__name">${name}</span>
+                <img class="products-element__img" src="${img}" />
+                <span class="products-element__price">💥 ${price.toLocaleString()} usd</span>
+                <button class="products-element__btn">В корзину</button>
             </li>`
         })
         const html = `
-            <ul>
+            <ul class="products-container">
                 ${htmlCatalog}
             </ul>
         `
