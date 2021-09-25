7ab4856705ee1d3ecd2f65aba0a8faa79d7d04f6
SKuznetzov <s-kuznecov100@rambler.ru>
2021-09-25 10:12:52 +0300

    let's create the localStorage class and a function for getting products from the storage

---
+++ utils/localStorageUtil.js
@@ -0,0 +1,16 @@
+class LocalStorageUtil {
+    constructor(){
+        this.keyName = 'products'
+    }
+}
+getProducts(){
+    const productsLocalStorage = localStorage.getItem(this.keyName)
+    if (productsLocalStorage !== null){
+        return JSON.parse(productsLocalStorage)
+    }
+    return []
+}
+
+putProducts(){
+
+}
