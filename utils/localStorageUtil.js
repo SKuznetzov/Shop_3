5e41c75f3a9ffcb1205838d5d5c4373ddce93e01
SKuznetzov <s-kuznecov100@rambler.ru>
2021-09-25 10:48:20 +0300

    adding elements

--- utils/localStorageUtil.js
+++ utils/localStorageUtil.js
@@ -11,7 +11,23 @@ getProducts(){
     return []
 }
 
-putProducts(){
+putProducts(id){
+    let products = this.getProducts()
+    let pushProduct = false
+    const index = products.indexOf(id)
+    if (index === -1){
+       products.push(id) 
+       pushProduct = true
+    } else {
+        products.splice(index,1)
+    }
+    localStorage.setItem(this.keyName,JSON.stringify(products))
+    return {
+        pushProduct: pushProduct,
+        products: products
+    }
+}
+}
 
-}
-}
+const localStorageUtil = new LocalStorageUtil()
+
