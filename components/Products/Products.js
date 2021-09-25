efe406270f1ada110a8fdd1026eb4aeb40a735d5
SKuznetzov <s-kuznecov100@rambler.ru>
2021-09-25 12:51:33 +0300

    add active buttons

--- components/Products/Products.js
+++ components/Products/Products.js
@@ -1,14 +1,29 @@
 class Products {
+    constructor(){
+        this.classNameActive = 'products-element__btn_active'
+        this.labelAdd = 'В корзину'
+        this.labelRemove = 'Удалить из корзины'
+    }
     render(){
+        const productsStore = localStorageUtil.getProducts()
         let htmlCatalog = ''
 
         CATALOG.forEach(({id,price,img,name})=>{
+            let activeClass = ''
+            let activeText = ''
+
+            if (productsStore.indexOf(id) === -1){
+                activeText = this.labelAdd
+            } else {
+                activeClass = ' '+this.classNameActive
+                activeText = this.labelRemove
+            }
             htmlCatalog +=`
             <li class="products-element">
                 <span class="products-element__name">${name}</span>
                 <img class="products-element__img" src="${img}" />
                 <span class="products-element__price">💥 ${price.toLocaleString()} usd</span>
-                <button class="products-element__btn">В корзину</button>
+                <button class="products-element__btn${activeClass}">${activeText}</button>
             </li>`
         })
         const html = `
