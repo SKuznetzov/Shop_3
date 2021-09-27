c671f059dc1e7f6dd630fd9b1f09671a0228888b
SKuznetzov <s-kuznecov100@rambler.ru>
2021-09-27 10:42:08 +0300

    add spinner img and setTimeout

--- index.js
+++ index.js
@@ -4,13 +4,19 @@ function render(){
     headerPage.render(productsStore.length)
     productsPage.render()
 }
+
+spinnerPage.render()
 let CATALOG = []
 
 fetch('server/catalog.json')
     .then(res =>res.json())
     .then(body =>{
         CATALOG = body
-        render()
+        setTimeout(()=>{
+            spinnerPage.handleClear()
+            render()
+
+        },2000)
     })
     .catch(error =>{
         console.log(error)
