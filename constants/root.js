8af61452333b805d98e07128b23642e9d9fe16ab
SKuznetzov <s-kuznecov100@rambler.ru>
2021-09-27 09:54:19 +0300

    add spinner

--- constants/root.js
+++ constants/root.js
@@ -1,3 +1,4 @@
 const ROOT_PRODUCTS = document.getElementById('products')
 const ROOT_HEADER = document.getElementById('header')
 const ROOT_CARD = document.getElementById('card')
+const ROOT_SPINNER = document.getElementById('spinner')
